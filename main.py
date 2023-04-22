e620067892e1b28fadd116d3c744347f6a9c3d01
N4YuC4 <yucelcan028@gmail.com>
2023-04-22 09:32:41 +0300

    menu eklendi

--- main.py
+++ main.py
@@ -1,31 +1,31 @@
+from bookshelf import * 
+
 """
 ___TO-DO___
 -sqlite entegre et
 """
 
-import sqlite3 as sl
-class bookshelf():
-    def __init__(self,ad,yayınevi,yazar):
-        self.ad=ad
-        self.yevi=yayınevi
-        self.yazar=yazar
+def tanımla():
+    global book
+    book=bookshelf(input("Kitap adı giriniz:"),input("Yayın evini giriniz:"),input("Yazarı giriniz:"))
 
-    @staticmethod
-    def tablo_olustur():
-        with sl.connect("kitaplık.db") as conn:
-            cursor = conn.cursor()
-            cursor.execute("""CREATE TABLE IF NOT EXISTS kitaplık(
-                            ad text,
-                            yayın_evi text,
-                            yazar text
-                        )""")
+def secim():
+    i =input("""
+    1)kitap ekle
+    2)kitap çıkar
+    3)listele
+    işlemi seçiniz:
+    """)
+    match i:
+        case "1":
+            tanımla()
+            bookshelf.ekle(book)
+            secim()
+        case "2":
+            bookshelf.sil()
+            secim()
+        case "3":
+            bookshelf.listele()
+            secim()
 
-    def ekle(self):
-        with sl.connect("kitaplık.db") as conn:
-            bookshelf.tablo_olustur()
-            cursor = conn.cursor()
-            cursor.execute("INSERT INTO kitaplık VALUES (?, ?, ?)", (self.ad, self.yevi, self.yazar))
-
-book=bookshelf(input("Kitap adı giriniz:"),input("Yayın evini giriniz:"),input("Yazarı giriniz:"))
-
-bookshelf.ekle(book)
+secim()
